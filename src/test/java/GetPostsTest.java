83ae4f863ab0f3a80cc1439634e53ee46c8517ff
Mohammed Shahin Uddin <mo@thetestroom.com>
2020-11-03 23:44:21 +0000

    Expanded test to cover more REST Assured methods

--- src/test/java/GetPostsTest.java
+++ src/test/java/GetPostsTest.java
@@ -1,15 +1,68 @@
 import org.junit.Test;
 
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 public class GetPostsTest {
 
+    /*
+    Given() lets you:
+        pass the request headers
+        query and path param
+        body
+        cookies and headers
+    When() lets you:
+        get
+        post
+        put
+    Then() lets you:
+        perform assertion conditions
+    */
+
+    @Test
+    public void shouldGetAllPost() {
+        given()
+            .get("https://jsonplaceholder.typicode.com/posts")
+        .then()
+            .body("id[0]", is(1));
+    }
+
     @Test
     public void shouldGetSpecificPost() {
-        when()
+        given()
             .get("https://jsonplaceholder.typicode.com/posts/1")
         .then()
-            .body("id", is(1));
+            .body("id", is(1))
+            .body("title", containsString("aut"));
+    }
+
+    @Test
+    public void shouldGetSpecificPostComment() {
+        given()
+            .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+        .then()
+            .body("id[0]", is(1))
+            .body("email[0]", is("Eliseo@gardner.biz"));
+    }
+
+    @Test
+    // Passing in URL parameters
+    public void shouldGetSpecificPostCommentPosts() {
+        given()
+            .queryParam("postId", 1)
+            .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+        .then()
+            .body("id[4]", is(5));
+    }
+
+    @Test
+    public void shouldBeAbleToPost() {
+        given()
+            .body("")
+        .when()
+            .post("https://jsonplaceholder.typicode.com/posts")
+        .then()
+            .body("", is(""));
     }
 }
