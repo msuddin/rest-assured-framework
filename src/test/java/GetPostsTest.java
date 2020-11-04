c499f4de958dd1c7fa3f213d3f3663b10321aaf7
Mohammed Shahin Uddin <mo@thetestroom.com>
2020-11-04 23:39:05 +0000

    Post tests including object passed into the body

--- src/test/java/GetPostsTest.java
+++ src/test/java/GetPostsTest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -19,6 +20,8 @@ public class GetPostsTest {
     Then() lets you:
         perform assertion conditions
     */
+
+    // GET EXAMPLES
 
     @Test
     public void shouldGetAllPost() {
@@ -56,13 +59,69 @@ public class GetPostsTest {
             .body("id[4]", is(5));
     }
 
+    // POST EXAMPLES
+
     @Test
-    public void shouldBeAbleToPost() {
+    // Using Java String
+    public void shouldBeAbleToPostUsingString() {
+        String payload = "{\"title\":\"hello_1\",\"body\":\"world_1\",\"userId\": 1}";
+
         given()
-            .body("")
+            .contentType(ContentType.JSON)
+            .body(payload)
         .when()
             .post("https://jsonplaceholder.typicode.com/posts")
         .then()
-            .body("", is(""));
+            .body("title", is("hello_1"));
+    }
+
+    @Test
+    // Using Java Object
+    public void shouldBeAbleToPostUsingObject() {
+        Post postObject = new Post("hello_2", "world_2", 2);
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(postObject)
+        .when()
+            .post("https://jsonplaceholder.typicode.com/posts")
+        .then()
+            .body("title", is("hello_2"));
+    }
+}
+
+class Post {
+    private String title;
+    private String body;
+    private int userId;
+
+    public Post(String title, String body, int userId) {
+        this.title = title;
+        this.body = body;
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 }
